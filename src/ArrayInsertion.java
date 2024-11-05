d371716025fe067769ca0f05b915698a71a749d0
Alberto <albertojunior848423803@gmail.com>
2024-11-05 13:29:22 +0300

    changes

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -7,7 +7,7 @@ public class ArrayInsertion {
         int my_Array[] = {10,20,30,40,50};
 
         //Define the position to substitute, position 3 of the array .
-        int Index_position = 2;
+        int index_position = 2;
 
         //Define the new value
         int new_Value = 35;
@@ -15,11 +15,14 @@ public class ArrayInsertion {
         System.out.println("original values of the before insertion : \n " + Arrays.toString(my_Array));
 
         //pushing array elementes from position 3 downwards
-        for (int i = my_Array.length-1; i>Index_position; i--){
+        for (int i = my_Array.length-1; i>index_position; i--){
             my_Array[i] = my_Array[i-1];
-
-            //Assigning value to array index 2
-
         }
+
+        //Assigning value to array index 2
+        my_Array[index_position] = new_Value;
+
+        //printing all elements after Insertion
+        System.out.println("New Array elements after Insertion of value 35: \n" + Arrays.toString(my_Array));
     }
 }
