241db4e75fa3612e8dcb045ba67dc7977daf9ab4
Alberto <albertojunior848423803@gmail.com>
2024-11-05 12:54:11 +0300

    changesch

--- src/arrayTraverse.java
+++ src/arrayTraverse.java
@@ -2,12 +2,5 @@ public class arrayTraverse {
 
     public static void main(String[] args) {
 
-        int LA[] = {10, 20, 30, 40, 50};
-
-        for (int i = 0; i<LA.length; i++){
-
-            System.out.printf("LA[%d] = %d \n", i, LA[i]);
-
-        }
     }
 }
