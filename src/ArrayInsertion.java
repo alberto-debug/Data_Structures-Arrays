24cad36776fdfa82f058946f8538e230901f9e71
Alberto <albertojunior848423803@gmail.com>
2024-11-05 14:03:01 +0300

    changes

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -4,26 +4,25 @@ public class ArrayInsertion {
 
     public static void main(String[] args){
 
-        int my_Array[] = {10,20,30,40,50};
+        int my_Array[]  = {10, 20, 30, 40, 50};
 
         //Define the position to substitute, position 3 of the array .
-        int index_position = 2;
+      int index_Position =  2 ;
 
         //Define the new value
         int new_Value = 35;
 
-        System.out.println("original values of the before insertion : \n " + Arrays.toString(my_Array));
+        System.out.println("Array elements before insertion: \n " + Arrays.toString(my_Array));
 
         //pushing array elementes from position 3 downwards
-        for (int i = my_Array.length-1; i>index_position; i--){
-            my_Array[i] = my_Array[i-1];
-        }
+      for (int i = my_Array.length-1; i>index_Position;i--){
+          my_Array[i] = my_Array[i-1];
+      }
 
         //Assigning value to array index 2
-        my_Array[index_position] = new_Value;
 
         //printing all elements after Insertion
-        System.out.println("New Array elements after Insertion of value 35: \n" + Arrays.toString(my_Array));
+
     }
 }
 //Objetivo do Loop: O loop percorre o array de trás para frente, movendo cada elemento uma
