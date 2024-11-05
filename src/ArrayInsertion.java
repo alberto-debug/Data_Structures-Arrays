c9e06b98eec16d4a8917051e9db0706eaf4494ed
Alberto <albertojunior848423803@gmail.com>
2024-11-05 13:07:19 +0300

    changes

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -2,6 +2,10 @@ public class ArrayInsertion {
 
     public static void main(String[] args){
 
+        int my_Array[] = {10,20,30,40,50};
 
+        //Define the position to substitute
+
+        //Define the new value
     }
 }
