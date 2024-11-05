05d7cc51f94f56ced0961115a24550afefaf4395
Alberto <albertojunior848423803@gmail.com>
2024-11-05 13:14:30 +0300

    changes

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 public class ArrayInsertion {
 
     public static void main(String[] args){
 
         int my_Array[] = {10,20,30,40,50};
 
-        //Define the position to substitute
+        //Define the position to substitute, position 3 of the array .
+        int Index_position = 2;
 
         //Define the new value
+        int new_Value = 35;
+
+        System.out.println("original values of the before insertion : \n " + Arrays.toString(my_Array));
     }
 }
