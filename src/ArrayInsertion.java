ef3b583092ca34040ca910951c45f0c2c6065977
Alberto <albertojunior848423803@gmail.com>
2024-11-05 13:33:20 +0300

    changes

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -26,3 +26,15 @@ public class ArrayInsertion {
         System.out.println("New Array elements after Insertion of value 35: \n" + Arrays.toString(my_Array));
     }
 }
+//Objetivo do Loop: O loop percorre o array de trás para frente, movendo cada elemento uma
+// posição para a direita. Isso cria um espaço vazio na posição index_position, onde o novo valor (new_Value) será inserido mais tarde.
+//
+//Início: int i = my_Array.length - 1
+//
+//O loop começa com i definido como o último índice do array.
+//Se my_Array tem 5 elementos ({10, 20, 30, 40, 50}), my_Array.length será 5, então i começa em 4 (my_Array.length - 1).
+//
+//Condição: i > index_position
+//
+//O loop continuará enquanto i for maior que index_position (neste caso, index_position = 2).
+//Isso significa que o loop vai parar assim que i atingir 2 + 1 = 3, pois queremos mover apenas os elementos depois de index_position.
