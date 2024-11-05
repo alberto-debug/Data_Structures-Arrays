e28f292f5c96711e374ade2623433c78a4f94e59
Alberto <albertojunior848423803@gmail.com>
2024-11-05 12:32:08 +0300

    commits

--- src/arrayTraverse.java
+++ src/arrayTraverse.java
@@ -1,4 +1,9 @@
 public class arrayTraverse {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        int LA[] = {10, 20, 30, 40, 50};
+
+        System.out.println("The original array values are: \n");
+    }
 }
