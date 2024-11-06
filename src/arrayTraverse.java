afd8484defe7733270cebfbca11337c941ca958e
Alberto <albertojunior848423803@gmail.com>
2024-11-06 12:48:51 +0300

    changes

--- src/arrayTraverse.java
+++ src/arrayTraverse.java
@@ -3,10 +3,12 @@ public class arrayTraverse {
     public static void main(String[] args) {
 
         // Initialize an integer array 'LA' with 5 elements
-
+        int LA[] = {10, 20, 30 , 40 , 50};
 
         // Loop through each element in the array using its index
+        for (int i = 0; i<LA.length;i++){
 
+        }
 
            // Print the index and value of each element in the array
 
