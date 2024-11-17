824829f29efbec1f518d1caa97266b1ff4b01ac1
Alberto <albertojunior848423803@gmail.com>
2024-11-17 22:39:11 +0300

    changes

--- src/arrayTraverse.java
+++ src/arrayTraverse.java
@@ -3,15 +3,16 @@ public class arrayTraverse {
     public static void main(String[] args) {
 
         // Initialize an integer array 'LA' with 5 elements
-        int LA[] = {10, 20, 30 , 40 , 50};
+        int LA[] = {10,20,30,40,50,60};
 
         // Loop through each element in the array using its index
-       for (int i =0;i<LA.length;i++){
+        for (int i = 0; i<LA.length;i++){
+            // Print the index and value of each element in the array
+            System.out.printf("LA[%d] = %d \n" , i, LA[i]);
+        }
 
-           // Print the index and value of each element in the array
-           System.out.printf("LA[%d] = %d \n", i , LA[i]);
 
-       }
+
 
 
     }
